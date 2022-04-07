5d025712ad630d734797eb1271104223000288f8
Sandra Ruiz <sandraa_so@hotmail.es>
2022-04-07 16:56:56 -0600

    Pokemon body nuevo

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App"> 
+      <header className="App-header">
+       <h1> POKEDEX </h1>
+       <br></br>
+       <input className="Buscador_Pokemon" placeholder='¿Quien es ese pokemon?'/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
